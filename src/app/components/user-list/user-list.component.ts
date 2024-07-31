5c6bac52cff89ef2a73342e76586f4b7068e063a
BaHGet <abosama150@gmail.com>
2024-07-31 21:52:32 +0300

    make user-list a standalone component and remove all console logs and import Ngif, NgFor

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../services/user.service';
+import { UserService, User } from '../../services/user.service';
+import { NgFor, NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  standalone: true,
+  imports: [NgIf, NgFor]
 })
 export class UserListComponent implements OnInit {
-  users: any[] = [];
+  users: User[] = [];
   page: number = 1;
   loading: boolean = false;
   error: string | null = null;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit() {
     this.loadUsers();
@@ -22,17 +26,12 @@ export class UserListComponent implements OnInit {
     this.loading = true;
     this.userService.getUsers(this.page).subscribe(
       (data) => {
-        console.log('User data received:', data);
         this.users = data;
         this.loading = false;
       },
       (error) => {
-        console.error('Error loading users:', error);
         this.error = 'Failed to load users';
         this.loading = false;
-      },
-      () => {
-        console.log('User data loading complete');
       }
     );
   }
@@ -50,6 +49,6 @@ export class UserListComponent implements OnInit {
   }
 
   viewUser(id: number) {
-    console.log(id)
+    this.router.navigate(['/user', id]);
   }
 }
