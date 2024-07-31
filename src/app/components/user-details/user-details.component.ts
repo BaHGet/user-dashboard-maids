5fdfb361b2c2eb32a5a750ea68e6f6036ae67d15
BaHGet <abosama150@gmail.com>
2024-07-31 18:30:03 +0300

    generate user-details componetnt and handel user details rendering

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as UserActions from '../../store/actions/user.actions';
+import { selectUser, selectLoading } from '../../store/selectors/user.selectors';
 
 @Component({
   selector: 'app-user-details',
-  standalone: true,
-  imports: [],
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.css'
+  styleUrls: ['./user-details.component.scss'],
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  user$: Observable<any>;
+  loading$: Observable<boolean>;
 
+  constructor(private store: Store, private route: ActivatedRoute, private router: Router) {
+    this.user$ = this.store.pipe(select(selectUser));
+    this.loading$ = this.store.pipe(select(selectLoading));
+  }
+
+  ngOnInit() {
+    const userId = this.route.snapshot.paramMap.get('id');
+    if (userId) {
+      this.store.dispatch(UserActions.loadUser({ id: +userId }));
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
