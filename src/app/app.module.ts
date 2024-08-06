264e4e5614738b8a16049dfedbed19b20efddcd1
BaHGet <abosama150@gmail.com>
2024-08-06 14:33:56 +0300

    import modules of angular materal and others modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import {HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 import { UserListComponent } from "./components/user-list/user-list.component";
 import { UserComponent } from "./components/user-details/user-details.component";
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
     declarations: [
@@ -16,11 +23,19 @@ import { UserComponent } from "./components/user-details/user-details.component"
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         RouterModule.forRoot([
             { path: '', component: UserListComponent },
             { path: 'user/:id', component: UserComponent },
         ]),
-        HttpClientModule
+        FormsModule,
+        HttpClientModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatGridListModule,
+        MatButtonModule,
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
