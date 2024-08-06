66f40053e481e643eb8d48536f1b315ec80d7213
BaHGet <abosama150@gmail.com>
2024-08-06 14:35:38 +0300

    add search functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SearchService } from './services/Search/search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent{
+  searchInput: string = '';
+  constructor(private searchService: SearchService) {}
+  
   onSearch(event:any) {
-    console.log(event.target.value);
+    this.searchInput = event.target.value;
+    this.searchService.updateSearchTerm(this.searchInput);
   }
 }
