5fa4a37e4e0621d5a45898f0577949a4bb081964
BaHGet <abosama150@gmail.com>
2024-08-06 11:47:57 +0300

    add UsersService and make it an non standalone component and handel getting user

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,32 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import * as UserActions from '../../store/actions/user.actions';
-import { selectUser, selectLoading } from '../../store/selectors/user.selectors';
+import { UsersService } from '../../services/users/user.service';
+import { User } from '../../models/user';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-user-details',
+  selector: 'app-user',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss'],
+  styleUrl: './user-details.component.css'
 })
-export class UserDetailsComponent implements OnInit {
-  user$: Observable<any>;
-  loading$: Observable<boolean>;
+export class UserComponent implements OnInit {
+  user: User;
+  
+  constructor(private router : ActivatedRoute,private userService : UsersService) { }
 
-  constructor(private store: Store, private route: ActivatedRoute, private router: Router) {
-    this.user$ = this.store.pipe(select(selectUser));
-    this.loading$ = this.store.pipe(select(selectLoading));
-  }
-
-  ngOnInit() {
-    const userId = this.route.snapshot.paramMap.get('id');
-    if (userId) {
-      this.store.dispatch(UserActions.loadUser({ id: +userId }));
-    }
-  }
-
-  goBack() {
-    this.router.navigate(['/']);
+  ngOnInit(): void {
+    this.router.params.subscribe((params) => {
+      const id: number = params['id'];
+      this.userService.getUser(id).subscribe((data: User) => {
+        // console.log(data);
+        this.user = data;
+      });
+    })
   }
 }
