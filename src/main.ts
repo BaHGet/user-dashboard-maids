8edc644515c3866fdc1e7e73aaca8cc7ad565b74
BaHGet <abosama150@gmail.com>
2024-08-06 11:51:09 +0300

    make the app component an non standalone component and wrap it in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app-routing.module'; 
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
-    provideRouter(routes)
-  ]
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
