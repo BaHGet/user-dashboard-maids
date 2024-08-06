e7c19436e67fc74f283d1e894685dc0415f9e2b3
BaHGet <abosama150@gmail.com>
2024-08-06 11:45:23 +0300

    make app component an non standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { UserListComponent } from './components/user-list/user-list.component';
-import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatInputModule,
-    UserListComponent
-  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'User Dashboard';
-
-  onSearch(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    const query = inputElement.value;
-    console.log('Search query:', query);
+  constructor() {}
+  onSearch(event:any) {
+    console.log(event.target.value);
   }
 }
